204fcd341e425bd8b74774d5a41f9842213130e1
Ben-Long50 <benjlong50@gmail.com>
2023-11-21 22:47:29 -0800

    added initial js and css features

--- script.js
+++ script.js
@@ -1,4 +1,43 @@
-const myLibrary = [];
+const myCompleteLibrary = [];
+const finishedLibrary = [];
+const unfinishedLibrary = [];
+const finishedBooksList = document.getElementById('finished-books-list');
+const unfinishedBooksList = document.getElementById('unfinished-books-list');
+
+const title = document.getElementById('title');
+const author = document.getElementById('author');
+const pages = document.getElementById('pages');
+const submitButton = document.getElementById('submit-button');
+const formButton = document.getElementById('form-button');
+const form = document.getElementById('form');
+const body = document.querySelector('body');
+
+formButton.addEventListener('click', function(){
+    if(form.classList.contains('hide-form')){
+        body.classList.toggle('expand-form');
+        form.classList.remove('hide-form');
+        form.classList.add('show-form');
+
+    }
+    else if(form.classList.contains('show-form')){
+        body.classList.toggle('expand-form');
+        form.classList.remove('show-form');
+        form.classList.add('hide-form');
+    }
+})
+
+submitButton.addEventListener('click', function(){
+    const statusInput = document.querySelector('input[name="status"]:checked');
+    new Book(title.value, author.value, pages.value, statusInput.id);
+    while (finishedBooksList.firstChild) {
+        finishedBooksList.removeChild(finishedBooksList.firstChild);
+    }
+    while (unfinishedBooksList.firstChild) {
+        unfinishedBooksList.removeChild(unfinishedBooksList.firstChild);
+    }
+    displayBook();
+    clearInputs();
+})
 
 function Book(title, author, pages, status){
     this.title = title;
@@ -9,9 +48,49 @@ function Book(title, author, pages, status){
 }
 
 function addBookToLibrary(book){
-    myLibrary.push(book);
+    myCompleteLibrary.push(book);
+    const status = book.status;
+    if(status === 'finished'){
+        finishedLibrary.push(book);
+    }
+    else if(status === 'unfinished'){
+        unfinishedLibrary.push(book);
+    }
 }
 
-const theFellowshipOfTheRing = new Book('The Fellowship of the Rings', 'JRR Tolkien', '432', 'read');
+function clearInputs(){
+    title.value = '';
+    author.value = '';
+    pages.value = '';
+}
 
-console.log(myLibrary);
+function displayBook(){
+    finishedLibrary.forEach(function(book){
+        const listItem = document.createElement('li');
+        const listTitle = document.createElement('h2');
+        const listAuthor = document.createElement('p');
+        const listPages = document.createElement('p');
+        listItem.classList.add('finished-book-card');
+        listTitle.textContent = book.title;
+        listAuthor.textContent = 'Written by: ' + book.author;
+        listPages.textContent = 'Number of Pages: ' + book.pages;
+        finishedBooksList.appendChild(listItem);
+        listItem.appendChild(listTitle);
+        listItem.appendChild(listAuthor);
+        listItem.appendChild(listPages);
+    })
+    unfinishedLibrary.forEach(function(book){
+        const listItem = document.createElement('li');
+        const listTitle = document.createElement('h2');
+        const listAuthor = document.createElement('p');
+        const listPages = document.createElement('p');
+        listItem.classList.add('finished-book-card');
+        listTitle.textContent = book.title;
+        listAuthor.textContent = 'Written by: ' + book.author;
+        listPages.textContent = 'Number of Pages: ' + book.pages;
+        unfinishedBooksList.appendChild(listItem);
+        listItem.appendChild(listTitle);
+        listItem.appendChild(listAuthor);
+        listItem.appendChild(listPages);
+    })
+}
